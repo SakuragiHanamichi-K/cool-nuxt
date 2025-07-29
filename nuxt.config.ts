a61e263cda5b19cd60f522a00d7b33d26f810091
yankun <751011138@qq.com>
2025-07-29 08:49:39 +0800

    Implement code refactoring for improved readability and maintainability

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ loadEnv({ path: `.env.${process.env.NODE_ENV}` })
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/image', '@nuxt/scripts', '@nuxt/test-utils', '@nuxt/ui'],
+  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/scripts', '@nuxt/test-utils', '@nuxt/ui'],
   runtimeConfig: {
     public: {
       appName: process.env.NUXT_PUBLIC_PROJECT_NAME,
