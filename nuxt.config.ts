4ad14bc37a05d08fb2a44662487aa9d5e0944088
yankun <w751011138@outlook.com>
2025-08-13 17:42:20 +0800

    fix(deps): 将 '@nuxt/ui-pro' 替换为 '@nuxt/ui'，更新相关依赖

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/scripts', '@nuxt/test-utils', '@nuxt/ui-pro'],
+  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/scripts', '@nuxt/test-utils', '@nuxt/ui'],
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [tailwindcss()],
